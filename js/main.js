c779f534b8c4fe939b138b23689bfc1285c49c4a
devgluszak <devgluszak@gmail.com>
2022-01-08 17:30:37 +0100

    navigation(part3), main.scss - create navItemsAnimation ,add class nav-items-animation, main.js - create function handleNavItemsAnimation()

--- js/main.js
+++ js/main.js
@@ -4,6 +4,7 @@ const allNawItems = document.querySelectorAll('.nav__item')
 
 const handleNav = () => {
 	nav.classList.toggle('nav--active')
+    handleNavItemsAnimation()
 }
 
 allNawItems.forEach(item => {
@@ -11,5 +12,15 @@ allNawItems.forEach(item => {
 		nav.classList.remove('nav--active')
 	})
 })
+
+const handleNavItemsAnimation = ()=>{
+    let delayTime = 0
+    allNawItems.forEach(item=>{
+        item.classList.toggle('nav-items-animation')
+        item.style.animationDelay=`.${delayTime}s`
+        //in css atribut animation-delay = 1s
+        delayTime++
+    })
+}
 
 navBtn.addEventListener('click', handleNav)
